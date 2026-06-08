440aa95f54e42b4f0a22c197a6d027a941e8ac96
Suraj Kumar <srsuraj009@gmail.com>
2026-06-08 16:58:03 +0530

    EDA And Feature Engineering is added

---
+++ demo.py
@@ -0,0 +1,10 @@
+from customer_churn.logger import logger
+
+logger.info("Application Started")
+
+a = 10
+b = 0
+
+result = a / b
+
+logger.info("Application Finished")
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="customer_churn",
+    version="0.0.1",
+    author="Suraj Kumar",
+    packages=find_packages(),
+)
